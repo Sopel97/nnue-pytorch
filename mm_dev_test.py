0e79348ef9248a9d589edbfa1a6600e91e4f9159
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-04-21 16:29:06 +0200

    cleaner backward

--- mm_dev_test.py
+++ mm_dev_test.py
@@ -214,6 +214,105 @@ void feature_transformer_slice_forward(
 
 ''', 'feature_transformer_slice_forward')
 
+feature_transformer_slice_backward = cp.RawKernel(r'''
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+
+extern "C" __global__
+/*
+    @assumptions:
+        The blocks must have dimensionality (BATCH_SIZE,)
+        The threads must have dimensionality (N,), where
+        N * output_thread_slice_size == output_size.
+
+    @param: feature_indices
+        A matrix of shape (BATCH_SIZE, max_active_features)
+        containing indices of active features for each position
+        in a batch. Feature index of -1 means that the slot is empty
+        and the weights will not be accumulated for it.
+        The indices form an implicit matrix of shape
+        (BATCH_SIZE, NUM_INPUTS), where the first dimension index is
+        inferred from the memory location (BATCH_SIZE), and the
+        second dimension index is stored in the feature_indices matrix.
+        The type for feature indices is int32_t.
+
+    @param: feature_values
+        A matrix of shape (BATCH_SIZE, max_active_features)
+        containing the values (arity) of the corresponding
+        feature index in feature_indices.
+        The type for the feature value (arity) is float32.
+
+    @param: max_active_features
+        The maximum number of features that are active
+        (non-zero) for a single position. This value determines
+        the shape of the inputs.
+        This value is of type uint32_t.
+
+    @param: weight_grad
+        The weight gradient matrix of shape (NUM_INPUTS, output_size).
+        The gradient is accumulated, i.e. it must be zero initialized
+        on the first call.
+        Weights must be of type float32.
+
+    @param: bias_grad
+        The bias gradient vector of shape (output_size,).
+        The gradient is accumulated, i.e. it must be zero initialized
+        on the first call.
+        Bias values must be of type float32.
+
+    @param: output_grad
+        An output gradient matrix of shape (BATCH_SIZE, output_size).
+        Output values must have type float32.
+
+    @param: output_size
+        The number of outputs. Must match the shape of weights
+        and biases.
+        This value is of type uint32.
+
+    @param: output_thread_slice_size
+        The number of output elements to be processed by a single thread.
+        It is assumed that N * output_thread_slice_size == output_size,
+        where N is the number of threads with which this kernel is launched.
+*/
+void feature_transformer_slice_backward(
+    const int32_t* feature_indices,
+    const float*   feature_values,
+    const int32_t  max_active_features,
+          float*   weight_grad,
+          float*   bias_grad,
+    const float*   output_grad,
+    const uint32_t output_size,
+    const uint32_t output_thread_slice_size
+) {
+    const uint32_t       block_idx         = blockIdx.x;
+    const uint32_t       slice_offset      = threadIdx.x * output_thread_slice_size;
+
+    const float*   const output_grad_slice = output_grad + block_idx * output_size + slice_offset;
+          float*   const bias_grad_slice   = bias_grad + slice_offset;
+
+    const int32_t* const feature_index_row = feature_indices + block_idx * max_active_features;
+    const float*   const feature_value_row = feature_values  + block_idx * max_active_features;
+
+    for (uint32_t s = 0; s < output_thread_slice_size; ++s)
+    {
+        atomicAdd(&bias_grad_slice[s], output_grad_slice[s]);
+    }
+
+    for (uint32_t k = 0; k < max_active_features; ++k) {
+        const int32_t feature_index = feature_index_row[k];
+        const float   feature_value = feature_value_row[k];
+        if (feature_index != -1) {
+            float* const weight_grad_slice = weight_grad + feature_index * output_size + slice_offset;
+            for (int s = 0; s < output_thread_slice_size; ++s) {
+                atomicAdd(&weight_grad_slice[s], output_grad_slice[s] * feature_value);
+            }
+        }
+    }
+}
+
+''', 'feature_transformer_slice_backward')
+
 BATCH_SIZE = 8192
 ITERS = 64
 
@@ -222,6 +321,7 @@ max_indices = 32
 weight = cp.random.rand(INPUT_SIZE, stride, dtype=cp.float32)
 bias = cp.random.rand(stride, dtype=cp.float32)
 weight_grad = cp.zeros((INPUT_SIZE, stride), dtype=cp.float32)
+bias_grad = cp.zeros((stride,), dtype=cp.float32)
 indices0 = (cp.random.rand(BATCH_SIZE, max_indices) * INPUT_SIZE).astype(cp.int32)
 indices1 = (cp.random.rand(BATCH_SIZE, max_indices) * INPUT_SIZE).astype(cp.int32)
 values0 = cp.random.rand(BATCH_SIZE, max_indices, dtype=cp.float32)
@@ -260,8 +360,38 @@ for i in range(ITERS):
             stride//num_threads
         )
     )
-    smm_backward((BATCH_SIZE,), (num_threads,), (indices0, values0, max_indices, weight_grad, output0 - output1, stride, stride//num_threads))
-    smm_backward((BATCH_SIZE,), (num_threads,), (indices1, values1, max_indices, weight_grad, output0 - output1, stride, stride//num_threads))
+
+    g = output0 - output1
+
+    feature_transformer_slice_backward(
+        (BATCH_SIZE,),
+        (num_threads,),
+        (
+            indices0,
+            values0,
+            max_indices,
+            weight_grad,
+            bias_grad,
+            g,
+            stride,
+            stride//num_threads
+        )
+    )
+    feature_transformer_slice_backward(
+        (BATCH_SIZE,),
+        (num_threads,),
+        (
+            indices1,
+            values1,
+            max_indices,
+            weight_grad,
+            bias_grad,
+            g,
+            stride,
+            stride//num_threads
+        )
+    )
+
     print(output0)
     print(output1)
     print(weight_grad)
